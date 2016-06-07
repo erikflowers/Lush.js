84d24fc1f0646157484d87b1f07b78c2fca571f4
Bob van Luijt <me@bob.technology>
2016-06-07 22:42:04 +0200

    Add tests

---
+++ tests/basic.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'Basic test for Lush' : function (browser) {
+    browser
+      .url('../examples/robotShop-full.html')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
+      .end();
+  }
+};
